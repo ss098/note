f38b4b67aa984de9aabea2a7d55ab1f45de8acdd
ss098 <ss098@qq.com>
2016-08-14 16:23:59 +0800

    修正 findById

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var Note = require("../models/note.js");
 
 /* GET home page. */
 router.get('/:id', function(req, res, next) {
-  var note = Note.find({"_id": ObjectId(req.params.id)});
+  var note = Note.findById(ObjectId(req.params.id));
   res.end(note);
   //res.render('index', { title: '深沉的情书与你' });
 });
