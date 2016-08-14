5e2b63f4d6286c9e20c30e306677862461bcee85
ss098 <ss098@qq.com>
2016-08-14 13:41:58 +0800

    修正 users.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '深沉的情书与你' });
 });
 
 module.exports = router;
