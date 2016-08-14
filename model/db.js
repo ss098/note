fd2181bcad12b9e0c2d8ab450b238fc434dc0ecc
ss098 <ss098@qq.com>
2016-08-14 16:11:04 +0800

    添加 mongoose

---
+++ model/db.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+mongoose.connect(process.env.MONGO_CONNECT_STRING);
+
+var noteSchema = new Schema({
+    title:  String,
+    content: String,
+    marodown: String,
+    date: { type: Date, default: Date.now },
+    password: String,
+});
+
+module.exports = mongoose.model('Note', noteSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var Note = require("../models/note.js");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: '深沉的情书与你' });
+router.get('/:id', function(req, res, next) {
+  var note = Note.find({"_id": ObjectId(req.params.id)});
+  res.end(note);
+  //res.render('index', { title: '深沉的情书与你' });
 });
 
 module.exports = router;
